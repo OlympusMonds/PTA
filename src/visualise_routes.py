fdbe1e1cbe5ebda8f85dc17088febab0ad3362ab
OlympusMonds <luke.s.mondy@gmail.com>
2016-04-15 23:27:12 +1000

    Visualiser now plots origin as black, and destinations as coloured points. The hotter the colour, the longer it takes to get to compared to driving.

--- src/visualise_routes.py
+++ src/visualise_routes.py
@@ -11,25 +11,44 @@ from database import Origin, Destination
 from database import init
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 
 
 def vis():
+    jet = cm = plt.get_cmap('jet')
+    cNorm  = colors.Normalize(vmin=0, vmax=1)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+
     with pny.db_session:
         origins = pny.select(o for o in Origin)[:]
 
         for o in origins:
             lat, lon = o.location.split(",")
-            plt.scatter(lon, lat, c="r", s=100)
+            plt.scatter(lon, lat, c="black", s=100)
 
             for d in o.destinations:
                 dlat, dlon = d.location.split(",")
-                plt.scatter(dlon, dlat)
-                plt.plot([lon, dlon], [lat, dlat], c="black")
+
+
+                count = 0
+                driving_dur = 0
+                transit_dur = 0
+                for t in d.trips:
+                    if t.mode == "driving":
+                        driving_dur = t.duration
+                    else:
+                        transit_dur += t.duration
+                        count += 1
+
+                transit_dur /= count
+
+                print driving_dur, transit_dur, driving_dur/transit_dur
+                colorVal = scalarMap.to_rgba(1 - (driving_dur/transit_dur))
+
+                plt.scatter(dlon, dlat, edgecolors="none", s=200, c=colorVal)
 
             plt.show()
-
-
-
 
 
 if __name__ == "__main__":
