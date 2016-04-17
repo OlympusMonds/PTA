d2bd8536c53ad75c223b51b8ed7b91d9609cb42e
OlympusMonds <luke.s.mondy@gmail.com>
2016-04-17 22:55:55 +1000

    Abstracted out bounding box and map res into main, for easier messing around.

--- src/main.py
+++ src/main.py
@@ -8,15 +8,19 @@ from threading import Thread
 
 
 def main():
-    url_queue = Queue(maxsize=10)   # If you make this too large, the times used can be
-                                    # in the past!
+    bounding_box = {"minlat": -33.846351, "minlon": 151.151910,
+                    "maxlat": -33.938762, "maxlon": 151.254523}
+    map_resolution = 3  # About 1 km
+
+    url_queue = Queue(maxsize=10)   # If you make this too large, the times used can be in the past!
+
     db = init()
 
     # Why did I make these things both threaded? It works, but it just seems unneeded
     # now... Particularly the route thread - why couldn't the url request thread just
     # call for a route just before it requests? Having the request threaded is OK,
     # particularly if I get more than 1 API key.
-    route_thread = Thread(target=generate_routes, args=(url_queue,))
+    route_thread = Thread(target=generate_routes, args=(bounding_box, map_resolution, url_queue,))
     url_request_thread = Thread(target=request_urls, args=(url_queue,))
 
     route_thread.start()
--- src/route_generator.py
+++ src/route_generator.py
@@ -2,27 +2,26 @@ import random
 from url_generator import get_urls_for_route
 
 
-def generate_routes(url_queue):
-    minlat, minlon = -33.846351, 151.151910
-    maxlat, maxlon = -33.938762, 151.254523
+def generate_routes(bounding_box, map_resolution, url_queue):
+    minlat, minlon = bounding_box["minlat"], bounding_box["minlon"]
+    maxlat, maxlon = bounding_box["maxlat"], bounding_box["maxlon"]
 
     deltalat = maxlat - minlat
     deltalon = maxlon - minlon
 
     routes = set()
-    route_resolution = 4
 
     while True:
         if len(routes) > 10e6:  # Ensure routes doesn't get too big..
             routes = set()
 
-        new_o_lat = round(minlat + random.random() * deltalat, route_resolution)
-        new_o_lon = round(minlon + random.random() * deltalon, route_resolution)
+        new_o_lat = round(minlat + random.random() * deltalat, map_resolution)
+        new_o_lon = round(minlon + random.random() * deltalon, map_resolution)
         origin = "{},{}".format(new_o_lat, new_o_lon)
 
         for _ in range(10):
-            new_d_lat = round(minlat + random.random() * deltalat, route_resolution)
-            new_d_lon = round(minlon + random.random() * deltalon, route_resolution)
+            new_d_lat = round(minlat + random.random() * deltalat, map_resolution)
+            new_d_lon = round(minlon + random.random() * deltalon, map_resolution)
             destination = "{},{}".format(new_d_lat, new_d_lon)
 
             data = get_urls_for_route(origin, destination)
