f0a74ba7ee08ef1a37b028eb361f71da89040225
OlympusMonds <luke.s.mondy@gmail.com>
2016-04-17 21:52:41 +1000

    Added some comments about the program structure.

--- src/main.py
+++ src/main.py
@@ -8,9 +8,14 @@ from threading import Thread
 
 
 def main():
-    url_queue = Queue(maxsize=1000)
+    url_queue = Queue(maxsize=10)   # If you make this too large, the times used can be
+                                    # in the past!
     db = init()
 
+    # Why did I make these things both threaded? It works, but it just seems unneeded
+    # now... Particularly the route thread - why couldn't the url request thread just
+    # call for a route just before it requests? Having the request threaded is OK,
+    # particularly if I get more than 1 API key.
     route_thread = Thread(target=generate_routes, args=(url_queue,))
     url_request_thread = Thread(target=request_urls, args=(url_queue,))
 
