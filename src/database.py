efd1f2144615900ad86b58818efbf3f52030a0d8
OlympusMonds <luke.s.mondy@gmail.com>
2016-04-17 21:51:53 +1000

    Added some comments about DB structure.

--- src/database.py
+++ src/database.py
@@ -4,6 +4,7 @@ from pony.orm import Database, Required, Set, PrimaryKey, sql_debug
 # What is up with the scoping of this..
 db = Database("sqlite", "database.sqlite", create_db=True)
 
+
 def init():
     sql_debug(False)
     db.generate_mapping(create_tables=True)
@@ -15,6 +16,14 @@ class Origin(db.Entity):
 
 
 class Destination(db.Entity):
+    """
+    This class maps back only one origin - yet, you could feasibly have
+    more than one origin pointing to the same destination (implying a
+    many to many relationship). However, at the moment, I can't see a
+    reason why it would be better do than the current setup, and one
+    imagines you would only care about where you start, rather than all
+    the potential starts at the end.
+    """
     id = PrimaryKey(int, auto=True)
     location = Required(str)
     origin = Required(Origin)
