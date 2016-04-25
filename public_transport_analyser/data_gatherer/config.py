bd85e1e5b388bcff2b7ee35cafae7f494415aa5f
Luke Mondy <luke.s.mondy@gmail.com>
2016-04-25 15:22:36 +1000

    allow more than one requester thread to be active, within the api restraints.

--- public_transport_analyser/data_gatherer/config.py
+++ public_transport_analyser/data_gatherer/config.py
@@ -4,5 +4,6 @@ bounding_box = {"minlat": -33.846351, "minlon": 151.151910,
 map_resolution = 3  # About 1 km
 
 max_daily_requests = 100000
+requester_threads = 1
 
 queue_size = 10  # If you make this too large, the times used can be in the past!
--- public_transport_analyser/data_gatherer/main.py
+++ public_transport_analyser/data_gatherer/main.py
@@ -6,7 +6,7 @@ from public_transport_analyser.database.database import init
 from public_transport_analyser.data_gatherer.route_generator import generate_routes
 from public_transport_analyser.data_gatherer.url_requester import request_urls
 from public_transport_analyser.data_gatherer.data_processor import process_data
-from public_transport_analyser.data_gatherer.config import bounding_box, map_resolution, max_daily_requests, queue_size
+from public_transport_analyser.data_gatherer.config import bounding_box, map_resolution, max_daily_requests, queue_size, requester_threads
 
 
 def main():
@@ -20,15 +20,14 @@ def main():
 
     # TODO: Migrate the route thread into a simple function call within the url thread.
     route_thread = Thread(target=generate_routes, args=(bounding_box, map_resolution, url_queue,))
-    url_request_thread = Thread(target=request_urls, args=(max_daily_requests, bad_routes, url_queue, data_queue))
-    data_processing_thread = Thread(target=process_data, args=(bad_routes, data_queue,))
-
-    # TODO: Implement logging
-    print("Starting threads... ", end="")
     route_thread.start()
-    url_request_thread.start()
+
+    for t in range(requester_threads):
+        url_request_thread = Thread(target=request_urls, args=(max_daily_requests/float(requester_threads), bad_routes, url_queue, data_queue))
+        url_request_thread.start()
+
+    data_processing_thread = Thread(target=process_data, args=(bad_routes, data_queue,))
     data_processing_thread.start()
-    print("done.")
 
     return 0
 
